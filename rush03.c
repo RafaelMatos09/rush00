bf80fa708ff64f07713068806e03f3b4c618346f
RafaelM <rafael.mattos09@gmail.com>
2023-07-30 22:54:03 -0300

    Update rush03.c

--- rush03.c
+++ rush03.c
@@ -5,13 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rafmorei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/30 20:04:51 by rafmorei          #+#    #+#             */
-/*   Updated: 2023/07/30 20:05:02 by rafmorei         ###   ########.fr       */
+/*   Created: 2023/07/30 21:36:45 by rafmorei          #+#    #+#             */
+/*   Updated: 2023/07/30 21:36:50 by rafmorei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include "ft_putchar.c"
+
+void	ft_putchar(c
+	if (x >= 1 && y >= 1 && x <= 4294967295 && y <= 4294967295)
+	{
+		while (linha <= y)
+		{
+			if (linha == 1)
+				imprime('A', 'B', 'C', x);
+			else if (linha == y)
+				imprime('A', 'B', 'C', x);
+			else
+				imprime('B', ' ', 'B', x);
+			linha++;
+		}
+	}
+	else
+	{
+		write(1, "Entrada inválida", 17);
+	}
+}
+har c);
 
 void	imprime(char ini, char meio, char fim, int tam)
 {
@@ -36,7 +56,7 @@ void	rush(int x, int y)
 	int	linha;
 
 	linha = 1;
-	if (x >= 1 && y >= 1 && x <= 99 && y <= 99)
+	if (x >= 1 && y >= 1 && x <= 4294967295 && y <= 4294967295)
 	{
 		while (linha <= y)
 		{
@@ -51,6 +71,6 @@ void	rush(int x, int y)
 	}
 	else
 	{
-		write(1, &"Entrada inválida", 17);
+		write(1, "Entrada inválida", 17);
 	}
 }
